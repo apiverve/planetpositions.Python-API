991b4ff6da0f4e5332e1ecc0e42162569b308d51
APIVerve Team <hello@apiverve.com>
2025-04-16 09:52:41 -0500

    Bug Fixes

---
+++ src/apiverve_planetpositions/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_planetpositions',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Planet Positions is a simple tool for getting the position of planets in the solar system. It returns the position of planets in the solar system.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
